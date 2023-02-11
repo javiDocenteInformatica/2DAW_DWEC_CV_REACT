e6cf491eec83682feae81bb011049f17d88e1013
javiDocenteInformatica <68152688+javiDocenteInformatica@users.noreply.github.com>
2023-02-11 23:07:45 +0100

    definiendo como almacenar un objeto JS en un fichero JSON para usarlo de base de datos
    sábado, 11 de febrero de 2023 23:07:41

--- apuntes_video/scripts/apuntes_video.js
+++ apuntes_video/scripts/apuntes_video.js
@@ -9,25 +9,27 @@ const urlYoutube = "https://youtu.be/6Jfk8ic3KVk?list=PLbaI3dQZLK5BtbhnPRflnjMZL
 // EVENTO LOAD: espera a que cargue todo el DOM
 $(document).ready(function () {
 
-  // Añade enlace a tiempo concreto de YouTube a cada botón de 'cartita'
-  f_eventoClickEnlaceYoutube();
-
-  // AÑADE BOOTSTRAP
-  // añade el BootStrap a cada carta
-  f_anyadeBootStrapCartita();
-
-  // JSON
-  f_generalJSON();
-
-  // CONTADOR Cartitas
-  f_contadorCartitas();
-
-
   // botón flecha para bajar al final del todo
   f_scrollBottomButton();
 
   // botón flecha para subir al principio del todo
   f_scrollTopButton();
+
+  // AÑADE BOOTSTRAP
+  // añade el BootStrap a cada carta
+  f_anyadeBootStrapCartita();
+
+  // CONTADOR Cartitas
+  f_contadorCartitas();
+
+  // Añade enlace a tiempo concreto de YouTube a cada botón de 'cartita'
+  f_eventoClickEnlaceYoutube();
+
+
+
+  // JSON
+  f_generalJSON();
+
 
 
 
--- apuntes_video/scripts/json.js
+++ apuntes_video/scripts/json.js
@@ -1,5 +1,5 @@
 
-const arrayCartitasSerializado = [];
+const arrayCartitasSerializado = {};
 
 
 const f_generalJSON = function () {
@@ -17,7 +17,7 @@ const f_generalJSON = function () {
       // JSON
       const cartitaSerializada = f_serializaHTML(this)
       //claves de propiedades computadas (JS) ['cartita' + indice]: https://www.benmvp.com/blog/learning-es6-enhanced-object-literals/
-      arrayCartitasSerializado.push({ ['cartita' + indice]: cartitaSerializada });
+      arrayCartitasSerializado[['cartita' + indice]] = cartitaSerializada;
 
 
     }// fin: function (nodo, indice, array)
@@ -26,14 +26,25 @@ const f_generalJSON = function () {
   );// fin: listaCartitas.forEach
 
   console.log(arrayCartitasSerializado);
+
+
+  // Esta función me debe permitir guardar un objeto JS en un fichero .json
+  f_guardarObjetoEnFichero();
+
+
+
+
+
+
 };
 
 
-/*
-FUNCION: 
-Te devuelve un objeto JS con una clave y como valor el elementoHTML serializado.
-Básicamente para serializar el elementoHTML usamos la propiedad 'outerHTML' del elemento HTML, que nos devolverá el código HTML del elemento como cadena.
-*/
+/**
+ * Función que serializa un elemento HTML (lo transforma en cadena).
+ * Básicamente para serializar el elementoHTML usamos la propiedad 'outerHTML' del elemento HTML, que nos devolverá el código HTML del elemento como cadena.
+ * @param {HTMLDivElement} elementoHTML
+ * @returns {string} cadenaHTML
+ */
 const f_serializaHTML = function (elementoHTML) {
   // tipo del elemento: https://stackoverflow.com/questions/332422/get-the-name-of-an-objects-type
   // console.log(elementoHTML.constructor.name);
@@ -47,5 +58,14 @@ const f_serializaHTML = function (elementoHTML) {
 
   return cadenaHTML;
 
+
+};
+
+/**
+ * Función que recibe un objeto JS y lo almacena en un fichero JSON.
+ * @param {Object} objetoJS
+ * @returns {void} no return
+ */
+const f_guardarObjetoEnFichero = function (objetoJS) {
 
 };
